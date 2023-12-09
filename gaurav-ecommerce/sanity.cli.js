62262cecfe06a30a32341e28c79b40d7aabf5b03
Gaurav Madan <gauravmadan2004@gmail.com>
2023-12-10 00:26:33 +0530

    sanity setup

---
+++ gaurav-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'km22c3gb',
+    dataset: 'production'
+  }
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { Navbar } from "../components";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-[10vh] text-center my-[4vh]">Hello World</h1>
+      </div>
+      <Navbar />
+    </>
+  );
 }
